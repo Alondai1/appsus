4f9279fdcb5c2900486d817b342cb6876ca3103e
Dima Leshin <dmitryleshin@gmail.com>
2019-06-20 19:51:51 +0300

    finished message form

--- js/apps/mail/cmps/create-mail.cmp.js
+++ js/apps/mail/cmps/create-mail.cmp.js
@@ -1,14 +1,19 @@
 export default {
     template: `
     <section class="mail-form">
-        <div class="mail-form-title">New message</div>
-        <form action="">
-            <ui-textbox label="To:" v-model="sentFrom" :floatingLabel="true"></ui-textbox>
-            <ui-textbox label="Subject:" v-model="subject" :floatingLabel="true"></ui-textbox>
-            <ui-textbox label="Message:" v-model="message" :floatingLabel="true" :multiLine="true"></ui-textbox>
+        <div class="mail-form-title flex">
+            <div>New message</div>
+            <div class="close-form" @click="deleteForm"><i class="fas fa-times"></i></div>
+        </div>
+        <form action="" class="flex">
             <div>
+                <ui-textbox label="To:" v-model="sentFrom" :floatingLabel="true"></ui-textbox>
+                <ui-textbox label="Subject:" v-model="subject" :floatingLabel="true"></ui-textbox>
+                <ui-textbox label="Message:" v-model="message" :rows="7" :floatingLabel="true" :multiLine="true"></ui-textbox>
+            </div>
+            <div class="mail-btns flex">
                 <ui-button @click.prevent size="normal" type="primary"color="primary">Send</ui-button>
-                <button @click.prevent><i class="fas fa-trash"></i></button>
+                <button @click.prevent="deleteForm"><i class="fas fa-trash"></i></button>
             </div>
         </form>
     </section>
@@ -18,6 +23,11 @@ export default {
             sentFrom: '',
             subject: '',
             message: '',
+        }
+    },
+    methods: {
+        deleteForm() {
+            this.$emit('deleteForm')
         }
     },
 }
