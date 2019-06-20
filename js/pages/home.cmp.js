3e398f3dcf76cec6526644fecdc0e2e8ce9c22c0
Dima Leshin <dmitryleshin@gmail.com>
2019-06-20 13:38:41 +0300

    little index changes

--- js/pages/home.cmp.js
+++ js/pages/home.cmp.js
@@ -1,13 +1,17 @@
 export default {
     template: `
-    <header class="home-header">
-        <h1>Welcome to Book Shop</h1>
-        <nav>
-            <router-link exact to="/">Home</router-link>
-            <router-link to="/about">About us</router-link>
-            <router-link to="/mail">Mail App</router-link>
-            <router-link to="/keep">Keep App</router-link>
-        </nav>
-    </header>
+    <section class="home-page">
+        <header class="home-header">
+            <h1>Welcome to Book Shop</h1>
+            <nav>
+                <router-link exact to="/">Home</router-link>
+                <router-link to="/about">About us</router-link>
+                <router-link to="/mail">Mail App</router-link>
+                <router-link to="/keep">Keep App</router-link>
+            </nav>
+        </header>
+        <main>
+        </main>
+    </section>
     `
 }
