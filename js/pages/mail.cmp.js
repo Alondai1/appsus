b99fecd0e984dd6ce0a102f074cf49c7f8187469
Dima Leshin <dmitryleshin@gmail.com>
2019-06-20 15:57:16 +0300

    added mail menu

--- js/pages/mail.cmp.js
+++ js/pages/mail.cmp.js
@@ -1,5 +1,6 @@
 import menu from '../apps/cmps/menu-header.cmp.js'
 import mailList from '../apps/mail/cmps/list.mail.cmp.js'
+import mailBar from '../apps/mail/cmps/mail-bar.cmp.js'
 
 
 
@@ -9,12 +10,27 @@ export default {
     <menu-header></menu-header>
         <h1>Mail Page</h1>
         <router-link to="/mail/">Mail Info</router-link>
+        <mail-bar @setFolder="showFolder"></mail-bar>
         <list-mail> </list-mail>
     </section>
     `,
+    data() {
+        return {
+            folder: '',
+        }
+    },
+    methods: {
+        showFolder(data) {
+            this.folder = data
+            console.log(this.folder);
+
+        }
+
+    },
 
     components: {
         'menu-header': menu,
-        'list-mail': mailList
+        'list-mail': mailList,
+        'mail-bar': mailBar
     }
 }
