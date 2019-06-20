08603ef6e9c674d42edec1ba40f4e40b96fc9140
Dima Leshin <dmitryleshin@gmail.com>
2019-06-20 17:29:29 +0300

    styling mail bar

--- js/apps/mail/cmps/mail-bar.cmp.js
+++ js/apps/mail/cmps/mail-bar.cmp.js
@@ -1,13 +1,14 @@
 export default {
     template: `
     <section class="mail-bar">
-        <div><i class="fas fa-plus"></i> compose</div>
+        <div class="mail-compose" @click="selectedFolder('compose')"><i class="fas fa-plus"></i> compose</div>
         <ul>
             <li @click="selectedFolder('inbox')"><i class="fas fa-inbox"></i> Inbox</li>
             <li @click="selectedFolder('starred')"><i class="fas fa-star"></i> Starred</li>
             <li @click="selectedFolder('sent')"><i class="fas fa-share-square"></i> Sent</li>
-            <li @click="selectedFolder('trash')"><i class="fas fa-share-square"></i> Trash</li>
+            <li @click="selectedFolder('trash')"><i class="fas fa-trash"></i> Trash</li>
         </ul>
+        <ui-progress-linear color="primary" type="determinate" :progress="75"></ui-progress-linear>
     </section>
     `,
     methods: {
