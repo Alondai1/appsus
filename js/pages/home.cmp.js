944842e3388fd94c56472ee11806855c5717e7fe
Alon Dai <alondai1@gmail.com>
2019-06-22 18:08:16 +0300

    animations

--- js/pages/home.cmp.js
+++ js/pages/home.cmp.js
@@ -7,12 +7,16 @@ export default {
         <main>
             <section class="main-grid-menu">
                 <ul class="menu-list flex">
-                    <li><router-link to="/mail"><img src="./././img/gmail-icon.png"/><span>Mail App</span></router-link></li>
-                    <li><router-link to="/about"><img src="./././img/about.png"/><span>About Us</span></router-link></li>
-                    <li><router-link to="/keep"><img src="./././img/keep.png"/><span>Keep App</span></router-link></li>
+
+                
+                    <li class="animated fadeInLeft delay-0.7s"><router-link to="/mail"><img src="./././img/gmail-icon.png"/><span>Mail App</span></router-link></li>
+                    <li class="animated fadeIn delay-1s"><router-link to="/about"><img src="./././img/about.png"/><span>About Us</span></router-link></li>
+                    <li class="animated fadeInRight delay-1.2s"><router-link to="/keep"><img src="./././img/keep.png"/><span>Keep App</span></router-link></li>
                 </ul>
             </section>
         </main>
     </section>
     `
+
+
 }
