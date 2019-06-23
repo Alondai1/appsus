1c9a28fbbbf6d9fb6e50a42c501530224dccf923
Dima Leshin <dmitryleshin@gmail.com>
2019-06-23 15:11:11 +0300

    added input note

--- js/apps/keep/cmps/notes.add.cmp.js
+++ js/apps/keep/cmps/notes.add.cmp.js
@@ -1,8 +1,10 @@
+import keepService from '../services/keep.service.js'
+
 export default {
     props: ['noteTypes'],
     template: `
     <section class="note-add-note flex">
-        <input :type="fieldType" v-model="input" :placeholder="placeholder"/>
+        <input :type="fieldType" v-model="input" :placeholder="placeholder" @keyup.enter="addNote" />
         <div v-for="(noteType, idx) in noteTypes" @click="selectType(idx)"><i :class="noteType.icon"></i></div>
     </section>
     `,
@@ -19,8 +21,13 @@ export default {
     },
     methods: {
         selectType(idx) {
+            this.input = ''
             this.newNote.type = idx
             console.log('selected:', idx);
+        },
+        addNote() {
+            keepService.addNote(this.input, this.newNote)
+            console.log('note added', this.input);
         }
     },
     computed: {
